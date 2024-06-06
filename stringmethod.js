ed434aa17d5bbba567efff42448ba2b0373139e5
sushilathing <sushilathing671@gmail.com>
2024-06-05 19:38:35 -0700

    first commit

---
+++ stringmethod.js
@@ -0,0 +1,10 @@
+var text="I am currently involved in 5th sem of BSC.csit"
+console.log(text);
+//print the length of the string
+console.log("The length of text is",text.length);
+//print the character at index 5 of the given string
+console.log("The character at index 5 is:",text.charAt(5));S
+//print the character code at index 5 of the given string
+console.log("The character at index 5 is:",text.charCodeAt(5));
+//print the string substring
+console.log("The substring of text is:",text.substring(1,4));
